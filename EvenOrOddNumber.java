d8e045d2c685e38f72640b695c8393f0373b718f
Deepaknani07 <121151296+Deepaknani07@users.noreply.github.com>
2025-03-12 22:37:45 +0530

    Create EvenOrOddNumber.java

---
+++ EvenOrOddNumber.java
@@ -0,0 +1,17 @@
+public class EvenOrOddNumber {
+
+	public static void main(String[] args) {
+		int number=11;
+		
+		if (number%2==0)
+		{
+			System.out.println("It is an even number");
+			
+		}
+		else {
+			System.out.println("it is an odd number");
+		}
+
+	}
+
+}
